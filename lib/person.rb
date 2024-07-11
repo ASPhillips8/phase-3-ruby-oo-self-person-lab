badc56e4ab4cf64edaf4a1f71b7669dadce301d0
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-11 13:34:43 -0400

    initialize hygiene

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
-# your code goes here
+require "pry"
+
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
+  def initialize (name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(value)
+    @happiness = if value < 0
+                   0
+                 elsif value > 10
+                   10
+                 else
+                   value
+                 end
+  end
+
+
+end
+
